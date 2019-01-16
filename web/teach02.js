ec2f066875d4b607635b3a1985465dc8f165e5e1
Ethan Williams <wil15119@byui.edu>
2019-01-16 16:42:34 -0700

    removed commented code

--- web/teach02.js
+++ web/teach02.js
@@ -5,16 +5,3 @@ function cc() {
 	var color = document.getElementById("_theirColor");
 	document.getElementById("d1").style.color = color;
 }
-/function cc() {
-	var xhttp = new XMLHttpRequest();
-	xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status ==200){
-			document.getElementById("_theirColor").innerHTML = 
-			this.responseText;
-		}
-	};
-	xhttp.open("POST", "/", true);
-	xhttp.send();
-	document.getElementById("d1").style.color = color;
-}
