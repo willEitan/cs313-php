b9c79839f28b96e787357d412500111eb26bb884
Ethan Williams <wil15119@byui.edu>
2019-01-16 15:23:37 -0700

    work done on cc()

--- web/teach02.js
+++ web/teach02.js
@@ -2,6 +2,16 @@ function clicked() {
 	alert("Clicked!");
 }
 function cc() {
+	var xhttp = new XMLHttpRequest();
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status ==200){
+			document.getElementById("_theirColor").innerHTML = 
+			this.responseText;
+		}
+	};
+	xhttp.open("POST", "/", true);
+	xhttp.send();
 	var color = document.getElementById("_theirColor");
 	document.getElementById("d1").style.color = color;
 }
