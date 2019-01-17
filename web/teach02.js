e8ed31f1cc0089394ece46c85c8980482b1b2be8
Ethan Williams <wil15119@byui.edu>
2019-01-16 17:06:56 -0700

    added .innerHTML

--- web/teach02.js
+++ web/teach02.js
@@ -2,6 +2,6 @@ function clicked() {
 	alert("Clicked!");
 }
 function cc() {
-	var color = document.getElementById("_theirColor");
+	var color = document.getElementById("_theirColor").innerHTML;
 	document.getElementById("d1").style.color = color;
 }
