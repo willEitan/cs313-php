876960133874496c471c8d3abc40f86214f0ac19
Ethan Williams <wil15119@byui.edu>
2019-01-16 17:18:01 -0700

    rm value

--- web/teach02.js
+++ web/teach02.js
@@ -2,6 +2,6 @@ function clicked() {
 	alert("Clicked!");
 }
 function cc() {
-	var color = document.getElementById("_theirColor").value;
+	var color = document.getElementById("_theirColor");
 	document.getElementById("d1").style.background-color = color;
 }
