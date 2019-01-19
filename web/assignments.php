a6b4f9dc2ce5ee958bf219df5b0289a6512387da
Ethan Williams <wil15119@byui.edu>
2019-01-19 10:56:12 -0700

    Initial commit

---
+++ web/assignments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">'
+	<title>Ethan's CS313 assignments</title>
+	<link rel="stylesheet" type="text/css" href="assignments.css">
+</head>
+<body>
+	<em font-size="30">Coming soon!</em>
+</body>
+<footer>
+	&copy; 
+	<?php
+		$created = 2019;
+		$current = date('Y');
+		echo created . (($created != current) ? '-' . $current : '') . " Copyright";
+	?>
+</footer>
+</html>
