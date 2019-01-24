2baad4008d54c10648b94c3740ef13667c432176
Ethan Williams <wil15119@byui.edu>
2019-01-24 14:09:50 -0700

    moved to new directory

--- web/teach02.js
+++
@@ -1,7 +0,0 @@
-function clicked() {
-	alert("Clicked!");
-}
-function cc() {
-	var color = document.getElementById("_theirColor").value;
-	document.getElementById("d1").style.backgroundColor = color;
-}
