04f49fc026ccb70cc279471d3a462120383d7e67
Ethan Williams <wil15119@byui.edu>
2019-01-24 19:25:46 -0700

    commnets

--- web/browse.php
+++ web/browse.php
@@ -60,9 +60,9 @@
 <footer>
 <!-- 	&copy; 
 	<?php
-		$created = 2019;
+		/*$created = 2019;
 		$current = date('Y');
-		echo $created . (($created != $current) ? '-' . $current : '') . " Copyright";
+		echo $created . (($created != $current) ? '-' . $current : '') . " Copyright";*/
 	?> -->
 </footer>
 </html>
