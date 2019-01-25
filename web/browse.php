400792d448580066ba398d829aafcda58cc9ae6a
Ethan Williams <wil15119@byui.edu>
2019-01-24 19:17:50 -0700

    commnets

--- web/browse.php
+++ web/browse.php
@@ -29,7 +29,7 @@
 	<title>ecommerce</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
 	<link rel="stylesheet" type="text/css" href="style_03.css">
 	<script type="text/javascript" src="myscript_03.js"></script>
 </head>
