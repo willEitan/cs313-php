b59d48af232efaba7854e910c77b24aeee2be25c
Ethan Williams <wil15119@byui.edu>
2019-01-25 22:40:07 -0700

    changes to the php

--- web/add_to_cart.php
+++ web/add_to_cart.php
@@ -23,9 +23,9 @@
 				$_SESSION['cart'] = $cart;
 				//array_push($_SESSION['cart'], $el);
 			}
-			//header('Location: browse.php');
-			//exit();
-			//echo $el . " - " . $_SESSION['cart'] . " ";
+			header('Location: browse.php');
+			exit();
+			echo $el . " - " . $_SESSION['cart'] . " ";
 			print_r($_SESSION['cart']);
 			//include(browse.php);
 			break;
