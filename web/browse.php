6223182dfc6bc78b4b864b9f849ac557a6eff2a6
Ethan Williams <wil15119@byui.edu>
2019-01-25 18:04:00 -0700

    significant changes to file. HTML built for image gallary

--- web/browse.php
+++ web/browse.php
@@ -1,33 +1,46 @@
 <?php
 	session_start();
 
-	$_SESSION['cart'] = [];
+	//$_SESSION['cart'] = [];
 	$_SESSION['artwork'] = [	'a1' => [ 	'id' => 'al',
 											'value' => 'Emma',
 											'src' => 'pics/emma.jpg',
 											'data-price' => '50',
-											'data-description' => "" ],
+											'data-description' => "",
+											'data-quantity'	=> 0],
 								'a2' => [ 	'id' => 'a2',
 											'value' => 'Oil Landscape',
 											'src' => 'pics/oil_landscape.jpg',
 											'data-price' => '$50.00',
-											'data-description' => "" ],	
+											'data-description' => "",
+											'data-quantity'	=> 0 ],	
 								'a3' => [ 	'id' => 'a3',
 											'value' => 'Miss Artist',
 											'src' => 'pics/sarah.jpg',
 											'data-price' => '$70.00',
-											'data-description' => "" ],
+											'data-description' => "",
+											'data-quantity' => 0 ],
 								'a4' => [ 	'id' => 'a4',
 											'value' => 'House in the Woods',
 											'src' => 'pics/hitw.jpg',
 											'data-price' => '$40.00',
-											'data-description' => "" ],	
+											'data-description' => "",
+											'data-quantity'	=> 0 ],	
 								'a5' => [ 	'id' => 'a5',
 											'value' => 'Unseen Protection',
 											'src' => 'pics/unseen_protection.jpg',
 											'data-price' => '$60.00',
-											'data-description' => "" ]					
+											'data-description' => "",
+											'data-quantity'	=> 0 ]
+								'a6' => [ 	'id' => 'a6',
+											'value' => 'Castle on a Hill',
+											'src' => 'pics/castle.jpg',
+											'data-price' => '$45.00',
+											'data-description' => "",
+											'data-quantity'	=> 0 ]	
+	//$_SESSION['user'] = [];				
 						];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,37 +55,113 @@
 <body>
  	<header>
 		<div class="top">
-			<h1>Sarah's Artwork</h1>
-			<div>
-				<form action="cart.php" method="post">
-					<input type="submit" name="cart button">
-				</form>
-			</div>
+			<h1>Shop Art</h1>
+			<h2>By Sarah Tenney</h2>
 		</div>
 	</header>
-	<div class="item-browse">
-		<ul class="items">
-			<div class="product-details">
-		 		<?php
-		 			$artwork = $_SESSION['artwork'];
-		 			$toAdd = [];
-		 			foreach ($artwork as $art) {
-		 				$src = $art['src'];
-		 				$alt = $art['value'];
-		 				$price = $art['data-price'];
-		 				echo "<div class='product'><li class='featured'>" . "<img src='$src' alt='$alt'><br>" . $price . "</li></div><br>" . "<div class='add-to-cart'><form method='post'><input type='button' id='click' value='Add to Cart' /></form></div><br>";
-		 			}
-		 			if(array_key_exists('click', $_POST)){
-		 					array_push($toAdd, $art);
-		 				}
-		 			$_SESSION['cart'] = $toAdd;
-		 		?>
-			</div> 
-			<hr>
-		</ul>		
+	<div class="art-gallary">
+		<div class="art-featured">.
+			<form method="post" action="/cart.php">
+				<!-- a1 -->
+				<div class="art-slide">
+		 			<div class="slide-info"> 1 / 6 <br>
+		 				<?php
+		 					echo $_SESSION['artwork']['a1']['data-price'];
+		 				?>
+		 				<input type="button" name="Add to Cart" onclick="<?php $_SESSION['artwork']['a1']['data-quantity'] += 1;?>" />
+		 			</div>
+		 			<img src="<?php echo $_SESSION['artwork']['a1']['src'];?>" alt="<?php echo $_SESSION['artwork']['a1']['value'];?>" style="width:100%;  height:50px;">
+				</div>
+				<!-- a2 -->
+				<div class="art-slide">
+		 			<div class="slide-info"> 2 / 6 <br>
+		 				<?php
+		 					echo $_SESSION['artwork']['a2']['data-price'];
+		 				?>
+		 				<input type="button" name="Add to Cart" onclick="<?php $_SESSION['artwork']['a2']['data-quantity'] += 1;?>" />
+		 			</div>
+		 			<img src="<?php echo $_SESSION['artwork']['a2']['src'];?>" alt="<?php echo $_SESSION['artwork']['a2']['value'];?>" style="width:100%;  height:50px;">
+				</div>
+				<!-- a3 -->
+				<div class="art-slide">
+		 			<div class="slide-info"> 3 / 6 <br>
+		 				<?php
+		 					echo $_SESSION['artwork']['a3']['data-price'];
+		 				?>
+		 				<input type="button" name="Add to Cart" onclick="<?php $_SESSION['artwork']['a3']['data-quantity'] += 1;?>" />
+		 			</div>
+		 			<img src="<?php echo $_SESSION['artwork']['a3']['src'];?>" alt="<?php echo $_SESSION['artwork']['a3']['value'];?>" style="width:100%;  height:50px;">
+				</div>
+				<!-- a4 -->
+				<div class="art-slide">
+		 			<div class="slide-info"> 4 / 6 <br>
+		 				<?php
+		 					echo $_SESSION['artwork']['a4']['data-price'];
+		 				?>
+		 				<input type="button" name="Add to Cart" onclick="<?php $_SESSION['artwork']['a4']['data-quantity'] += 1;?>" />
+		 			</div>
+		 			<img src="<?php echo $_SESSION['artwork']['a4']['src'];?>" alt="<?php echo $_SESSION['artwork']['a4']['value'];?>" style="width:100%;  height:50px;">
+				</div>
+				<!-- a5 -->
+				<div class="art-slide">
+		 			<div class="slide-info"> 5 / 6 <br>
+		 				<?php
+		 					echo $_SESSION['artwork']['a5']['data-price'];
+		 				?>
+		 				<input type="button" name="Add to Cart" onclick="<?php $_SESSION['artwork']['a5']['data-quantity'] += 1;?>" />
+		 			</div>
+		 			<img src="<?php echo $_SESSION['artwork']['a5']['src'];?>" alt="<?php echo $_SESSION['artwork']['a5']['value'];?>" style="width:100%;  height:50px;">
+				</div>
+				<!-- a6 -->
+				<div class="art-slide">
+		 			<div class="slide-info"> 6 / 6 <br>
+		 				<?php
+		 					echo $_SESSION['artwork']['a6']['data-price'];
+		 				?>
+		 				<input type="button" name="Add to Cart" onclick="<?php $_SESSION['artwork']['a6']['data-quantity'] += 1;?>" />
+		 			</div>
+		 			<img src="<?php echo $_SESSION['artwork']['a6']['src'];?>" alt="<?php echo $_SESSION['artwork']['a6']['value'];?>" style="width:100%; height:50px;">
+				</div>
+
+				<!-- Browse Arrows -->
+				<a class="prev" onclick="plusSlides(-1);">❮</a>
+				<a class="next" onclick="plusSlides(1);">❯</a>
+
+				<div class="art-title">
+					<p id="caption"></p>
+				</div>
+
+				<!-- Image Row -->
+				<div class="row">
+					<div class="col">
+						<img src="<?php echo $_SESSION['artwork']['a1']['src'];?>" alt="<?php echo $_SESSION['artwork']['a1']['value'];?>" style="width:100%; height:20px;" onclick="currentSlide(1);">
+					</div>
+					<div class="col">
+						<img src="<?php echo $_SESSION['artwork']['a2']['src'];?>" alt="<?php echo $_SESSION['artwork']['a2']['value'];?>" style="width:100%; height:20px;" onclick="currentSlide(2);">
+					</div>
+					<div class="col">
+						<img src="<?php echo $_SESSION['artwork']['a3']['src'];?>" alt="<?php echo $_SESSION['artwork']['a3']['value'];?>" style="width:100%; height:20px;" onclick="currentSlide(3);">
+					</div>
+					<div class="col">
+						<img src="<?php echo $_SESSION['artwork']['a4']['src'];?>" alt="<?php echo $_SESSION['artwork']['a4']['value'];?>" style="width:100%; height:20px;" onclick="currentSlide(4);">
+					</div>
+					<div class="col">
+						<img src="<?php echo $_SESSION['artwork']['a5']['src'];?>" alt="<?php echo $_SESSION['artwork']['a5']['value'];?>" style="width:100%; height:20px;" onclick="currentSlide(5);">
+					</div>
+					<div class="col">
+						<img src="<?php echo $_SESSION['artwork']['a6']['src'];?>" alt="<?php echo $_SESSION['artwork']['a6']['value'];?>" style="width:100%; height:20px;" onclick="currentSlide(6);">
+					</div>
+				</div>
+				<br><input type="submit" name="Checkout" />
+			</form>
+			<script type="text/javascript" src="gallary.js"></script>
+		</div>
+		<hr>
 	</div>
+	<script type="text/javascript" src="gallary.js"></script>
 </body>
 <footer>
+	<hr>
 	&copy; 
 	<?php
 		$created = 2019;
