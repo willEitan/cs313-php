3518f6efba3af2496c300c7ce92a79d2ad0b3da4
Ethan Williams <wil15119@byui.edu>
2019-01-26 22:46:20 -0700

    address

--- web/service.php
+++ web/service.php
@@ -1,6 +1,44 @@
 <?php
 	session_start();
 
+
+	$_SESSION['artwork'] = [	'a1' => [ 	'id' => 'al',
+											'value' => 'Emma',
+											'src' => 'pics/emma.jpg',
+											'data-price' => 50.00,
+											'data-description' => "Emma Watson drawing",
+											'data-quantity'	=> 0],
+								'a2' => [ 	'id' => 'a2',
+											'value' => 'Oil Landscape',
+											'src' => 'pics/oil_landscape.jpg',
+											'data-price' => 50.00,
+											'data-description' => "Landscape Oil Painting",
+											'data-quantity'	=> 0 ],	
+								'a3' => [ 	'id' => 'a3',
+											'value' => 'Miss Artist',
+											'src' => 'pics/sarah.jpg',
+											'data-price' => 70.00,
+											'data-description' => "The Artist",
+											'data-quantity' => 0 ],
+								'a4' => [ 	'id' => 'a4',
+											'value' => 'House in the Woods',
+											'src' => 'pics/hitw.jpg',
+											'data-price' => 40.00,
+											'data-description' => "A Simple House in the Woods",
+											'data-quantity'	=> 0 ],	
+								'a5' => [ 	'id' => 'a5',
+											'value' => 'Unseen Protection',
+											'src' => 'pics/unseen_protection.jpg',
+											'data-price' => 60.00,
+											'data-description' => "Protection Unseen",
+											'data-quantity'	=> 0 ],
+								'a6' => [ 	'id' => 'a6',
+											'value' => 'Castle on a Hill',
+											'src' => 'pics/castle.jpg',
+											'data-price' => 45.00,
+											'data-description' => "A Castle and a King",
+											'data-quantity'	=> 0 ]];
+
 	$action = filter_input(INPUT_POST, 'action');
 	if ($action == NULL){
 		$action = filter_input(INPUT_GET, 'action');
@@ -23,7 +61,15 @@
 			exit();
 			break;
 		case "remove":
-
+			break;
+		case "address"
+			if (!isset($_SESSION['address'])) {
+				$_SESSION['address'] .= $_POST['Street Address'];
+				$_SESSION['address'] .= $_POST['City'];
+				$_SESSION['address'] .= ", ";
+				$_SESSION['address'] .= $_POST['ZIP'];
+			}
+			header('Location: confirmation.php')
 			break;
 	}				
 ?>
