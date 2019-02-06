98a1324c85d1bac9714a9e6287bf998121705e92
Ethan Williams <wil15119@byui.edu>
2019-02-06 12:06:46 -0700

    footer tag

--- web/footer.php
+++ web/footer.php
@@ -1,8 +1,10 @@
-<hr>
-&copy; 
-<?php
+<footer>
+	<hr>
+	&copy; 
+	<?php
 
-	$created = 2019;
-	$current = date('Y');
-	echo $created . (($created != $current) ? '-' . $current : '') . " Copyright";
-?> 
+		$created = 2019;
+		$current = date('Y');
+		echo $created . (($created != $current) ? '-' . $current : '') . " Copyright";
+	?> 
+</footer>
