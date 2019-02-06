cced9ccb5da21e598453dfebf819644b4c27624c
Ethan Williams <wil15119@byui.edu>
2019-02-05 20:30:11 -0700

    teach assignment

--- web/teamwork/teach_05.php
+++ web/teamwork/teach_05.php
@@ -21,6 +21,7 @@
 	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	  echo "db connected!";
 	}
 	catch (PDOException $ex)
 	{
