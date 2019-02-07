63ffe80ef77d377f0ec32447c0083194d1f3a84a
Ethan Williams <wil15119@byui.edu>
2019-02-06 17:38:14 -0700

    nav bar

---
+++ web/projects/shop_art/php/request.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Make request</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="shop_style.css">
+	<script type="text/javascript" src="shop.js"></script>
+	<?php require "db_connect.php";?>
+</head>
+<body>
+	<div include-html="../html/nav.html"></div>
+	<script>includeHTML();</script>
+
+	<div class="main-content">
+		<form method="post" action="confirmation.php">
+			<input type="text" name="">
+		</form>
+	</div>
+</body>
+<?php
+	include "footer.php";
+?>
+</html>
