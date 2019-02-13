ac357672e27cc812e4987ebf4ae83ed833b95fe5
Ethan Williams <wil15119@byui.edu>
2019-02-13 16:21:06 -0700

    add to cart functionality

--- web/projects/shop_art/php/checkout.php
+++ web/projects/shop_art/php/checkout.php
@@ -1,8 +1,9 @@
 <?php
 	session_start();
 	header("Content-Type: application/json; charset=UTF-8");
+	print_r($_GET["cs"]);
 	$obj = json_decode($_GET["cs"], false);
 	$_SESSION["ids"] = $obj->id;
 	$_SESSION["quantites"] = $obj->quantity;
-	header('Location: cart-checkout.php');
+	//header('Location: cart-checkout.php');
 ?>
