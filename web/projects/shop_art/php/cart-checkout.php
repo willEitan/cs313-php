c735355cc24490c809f78f166959c5890fc2be98
Ethan Williams <wil15119@byui.edu>
2019-02-15 18:45:13 -0700

    checkout page debuging

--- web/projects/shop_art/php/cart-checkout.php
+++ web/projects/shop_art/php/cart-checkout.php
@@ -89,22 +89,18 @@
 				<h4>Art Cart<span class="price" style="color:black"><i class="fa fa-shopping-cart"></i><b>
 					<?php 
 						$quantity = 0;
-						foreach($_SESSION["cart"] as $key => $value) {
-							echo $key . " " . $value . "<br>";
-							/*if ($key == "quantity" || $key % 2) {
-								echo "here<br>";
-								$quantity += $key[$value];
-							}*/
+						foreach($_SESSION["cart"] as $value) {
+							$quantity += (int)$value;
 						}
 						unset($value);
-						//echo $quantity;
+						echo $quantity;
 					?>
 					</b></span></h4>
 				<?php
 					$total = 0;
-					$query = $db->query('SELECT art_id, image, image_title, price FROM art WHERE {$key[$value]} = art_id ORDER BY image_title');
+					$query = $db->query('SELECT art_id, image, image_title, price FROM art WHERE {$key} = art_id ORDER BY image_title');
 
-					/*foreach ($_SESSION["cart"] as $key => $value) {
+					foreach ($_SESSION["cart"] as $key => $value) {
 						$results = $query->fetch(PDO::FETCH_ASSOC);
 						if ($results) {
 							echo "<p class='products'><a href='{$results[0]['image']}'></a>";
@@ -116,7 +112,7 @@
 							echo "<span class='price'>{$price}</span></p>";
 						}
 					}
-					unset($value);*/
+					unset($value);
 				?>
 				<hr>
 				<p>Total <span class="price" style="color:black"><b><?php echo $total; ?></b></span></p>
