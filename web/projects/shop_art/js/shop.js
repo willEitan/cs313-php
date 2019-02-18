dc7cfefa75a32291c711422d28fd4782043dd219
Ethan Williams <wil15119@byui.edu>
2019-02-18 12:58:56 -0700

    regex

--- web/projects/shop_art/js/shop.js
+++ web/projects/shop_art/js/shop.js
@@ -149,7 +149,7 @@ function valCname() {
     document.getElementById("errCname").style.display = none;
 }
 
-function valCnn () {
+function valCcn () {
   var field = document.getElementById("ccnum");
   /*code curtosy: https://stackoverflow.com/questions/9315647/regex-credit-card-number-tests*/
   var regex = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
