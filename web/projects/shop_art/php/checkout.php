890d7dafc575bd589b35ee8d56f6a7dce9210663
Ethan Williams <wil15119@byui.edu>
2019-02-22 14:42:28 -0700

    checkout processing

--- web/projects/shop_art/php/checkout.php
+++ web/projects/shop_art/php/checkout.php
@@ -31,16 +31,16 @@
 
 			echo $ccn . "<br>" . $cvv . "<br>" . $cname . "<br>" . $email;
 			$user_info = $db->query("INSERT INTO user_info (user_info_id, first_name, last_name, email, is_artist, creation_date, created_by, last_updated_by, last_update_date) VALUES (nextval(ui_seq, {$first}, {$last}, {$email}, FALSE, current_date, 1001, 1001, current_date))");
-			$address = $db->query("INSERT INTO address (address_id, street_address, city, state, postal_code, user_info_id, creation_date, created_by, last_updated_by, last_update_date) VALUES (nextval(ad_seq), {$adr}, {$city}, {$state}, {$zip}, {$pdo->lastInsertId(ui_seq);}, current_date, 1001, 1001, current_date");
-			$shopper = $db->query("INSERT INTO shopper (shopper_id, user_info_id, card_number_hash, card_cvv_hash, creation_date, created_by, last_updated_by, last_update_date, card_holder_name) VALUES (nextval(s_seq), {$pdo->lastInsertId(ui_seq);}, {$ccn}, {$cvv}, current_date, 1001, 1001, current_date)");
-			$purchase_order = $db->query("INSERT INTO purchase_order (purchase_order_id, shopper_id, ship_to_address_id, total, paid, payment_method_type, creation_date, created_by, last_updated_by, last_update_date, status) VALUES (nextval(o_seq), {$pdo->lastInsertId(s_seq);}, {$pdo->lastInsertId(ad_seq);}, {$_SESSION['total']}, FALSE, CREDIT, current_date, 1001, 1001, current_date)");
+			$address = $db->query("INSERT INTO address (address_id, street_address, city, state, postal_code, user_info_id, creation_date, created_by, last_updated_by, last_update_date) VALUES (nextval(ad_seq), {$adr}, {$city}, {$state}, {$zip}, {$pdo->lastInsertId(ui_seq)}, current_date, 1001, 1001, current_date");
+			$shopper = $db->query("INSERT INTO shopper (shopper_id, user_info_id, card_number_hash, card_cvv_hash, creation_date, created_by, last_updated_by, last_update_date, card_holder_name) VALUES (nextval(s_seq), {$pdo->lastInsertId(ui_seq)}, {$ccn}, {$cvv}, current_date, 1001, 1001, current_date)");
+			$purchase_order = $db->query("INSERT INTO purchase_order (purchase_order_id, shopper_id, ship_to_address_id, total, paid, payment_method_type, creation_date, created_by, last_updated_by, last_update_date, status) VALUES (nextval(o_seq), {$pdo->lastInsertId(s_seq)}, {$pdo->lastInsertId(ad_seq)}, {$_SESSION['total']}, FALSE, CREDIT, current_date, 1001, 1001, current_date)");
 
 			foreach ($_SESSION["cart"] as $key => $value) {
 				$query = $db->query("SELECT art_id, image, image_title, price FROM art WHERE art_id = '{$key}' ORDER BY image_title");
 				$results = $query->fetch(PDO::FETCH_ASSOC);
 
 				if ($results) {
-					$lookup = $db->query("INSERT INTO art_purchase_order_lookup (art_purchase_order_lookup_id, purchase_order_id, art_id, item_quantity, creation_date, created_by, last_updated_by, last_update_date) VALUES (nextval(aol_seq), {$pdo->lastInsertId(o_seq);}, {$key}, {$value}) ");
+					$lookup = $db->query("INSERT INTO art_purchase_order_lookup (art_purchase_order_lookup_id, purchase_order_id, art_id, item_quantity, creation_date, created_by, last_updated_by, last_update_date) VALUES (nextval(aol_seq), {$pdo->lastInsertId(o_seq)}, {$key}, {$value}) ");
 
 				}
 			}
